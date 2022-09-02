af542619f5a52ca525210f6e53a903c70a350dfa
Mickael PAPAMICA Asseline <mickael@papamica.com>
2022-09-02 10:18:08 +0200

    add list for curl

--- .github/workflows/generate-list.py
+++ .github/workflows/generate-list.py
@@ -6,12 +6,14 @@ from xml.etree.ElementTree import Element, SubElement, tostring, ElementTree
 GITHUB_REPOSITORY_URL = 'https://github.com/PAPAMICA/img.papamica.com'
 
 try:
-    os.remove("./list.xml") 
+    os.remove("./list.xml")
+    os.remove("./list") 
   #  os.remove("./README.md") 
 except:
     print ("file don't exist")
 
 imglist = Element('imglist')
+imglist2 = open('list', 'a')
 
 for filename in sorted(glob.glob("./*/*.*", recursive = True)):
     try:
@@ -21,6 +23,7 @@ for filename in sorted(glob.glob("./*/*.*", recursive = True)):
         name.text = os.path.basename(filename)
         link = SubElement(child, "link")
         link.text = filename[1:]
+        imglist2.write("https://img.papamica.com" + filename[1:] + "\n")
 
     except:
          print (f" ❌ {filename} error !")
