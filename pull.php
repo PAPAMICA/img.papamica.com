035bcb31051f65af9eab5910fdc50435197f292f
Mickael PAPAMICA Asseline <mickael@papamica.com>
2022-09-02 11:16:35 +0200

    fix pull.php

--- pull.php
+++ pull.php
@@ -1,4 +1,21 @@
 <?php
-echo shell_exec("git pull");
+
+//Store the output of the executed command
+
+$output1 =  exec('pwd');
+
+//Print the return value
+
+echo $output1;
+
+echo "<br/>";
+
+//Store the last line of the executed command
+
+$output2 = exec('ls -la');
+
+//Print the return value
+
+echo $output2;
 
 ?>
